833b16229b480792e1ff51c34a17da349a6279a0
connorfor <connofor@gmail.com>
2021-05-05 13:36:57 -0400

    extra files

---
+++ EFinance/Error/APIError.swift
@@ -0,0 +1,29 @@
+//
+//  APIError.swift
+//  StocksApp
+//
+//  Created by connor dong on 2021-05-02.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case decodingError
+    case errorCode(Int)
+    case unknown
+}
+
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .decodingError:
+            return "Failed to decode the object from the service"
+        case .errorCode(let code):
+            return "\(code) - Something went wrong"
+        case .unknown :
+            return "The error is unknown"
+            
+        
+        }
+    }
+}
